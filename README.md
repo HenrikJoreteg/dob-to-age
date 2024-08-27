44b06ef643b02940fec51f7df7749969548493a7
Henrik Joreteg <hjoreteg@gmail.com>
2024-08-27 14:35:13 -0700

    Relative date fix, and return object of values and units instead of just a number (#2)
    * updated: totally new technique and now returns object with years, months, or days depending on "standard" medical thresholds
    * 2.0.0

--- README.md
+++ README.md
@@ -2,20 +2,40 @@
 
 ![](https://img.shields.io/npm/dm/dob-to-age.svg)![](https://img.shields.io/npm/v/dob-to-age.svg)![](https://img.shields.io/npm/l/dob-to-age.svg)
 
-A single small function for turning a date of birth string as `YYYY`, `YYYY-MM`, or `YYYY-MM-DD` into an "age" in common terms. This is not a simple mathematical calculation based on the number of milliseconds in a year.
+A single small function for turning a date of birth string in the format `YYYY`, `YYYY-MM`, or `YYYY-MM-DD` into an "age" in common terms. This is not a simple mathematical calculation based on the number of milliseconds in a year.
 
-It turns out human logic for "age" is a little weird, this lib tries to match this logic.
+It turns out human logic for "age" is a little weird, and this library tries to match that logic.
 
-For example, if today is April 15, 2021 (`2021-04-15`)
-
-If your birth date is `2000-04-15`, it's your birthday! And regardless of what timezone or time of the day you were born, etc you are considered to be 21 on this day. To get this right, we can't simply use `new Date() - new Date('2000-04-15')` and calculate number of years by dividing whole years.
+For example, let's say today is April 15, 2021 (`2021-04-15`). If your birth date was `2000-04-15`, it's your birthday today! Regardless of what timezone or time of day you were born, you are considered to be 21 on this day. To get this right, we can't simply use `new Date() - new Date('2000-04-15')` and calculate the number of years by dividing by whole years.
 
 Instead, human logic says:
 
-- Always assume same time zone.
-- Assume if the day matches, you are n + 1 years old regardless of time.
+- Always assume the same time zone.
+- Assume that if the day matches, you are `n + 1` years old, regardless of the time.
 
-So that's what this lib does. See tests cases for more examples.
+Additionally, in medical settings, it's semi-standard to count days up until 59 days and then switch to months. The values are returned in months up until 23 months; after that, they are returned in years.
+
+This library matches that (somewhat screwy) logic.
+
+It _always_ returns an object.
+
+The object will have only one of the following:
+
+If the result should be shown in years:
+
+`{years: 2}`
+
+If the result should be shown in months:
+
+`{months: 21}`
+
+If the result should be shown in days (because we're under 2 months):
+
+`{days: 18}`
+
+If it gets any invalid input or errors, it will fail silently and just return an empty object. This is to make it easy to handle the result for rendering an age.
+
+`{}`
 
 ## install
 
@@ -28,7 +48,9 @@ npm install dob-to-age
 ```js
 import dobToAge from 'dob-to-age'
 
-dobToAge('1982-09-29') // 38 (at time of writing this)
+dobToAge('1982-09-29') // {years: 38} (at time of writing this)
+// can pass a reference date object
+dobToAge('1982-09-29', new Date('1982-10-29')) // {days: 30}
 ```
 
 # test
@@ -39,6 +61,7 @@ npm test
 
 ## Change log
 
+- `2.0.0`: New implementation, returns object of either years, months, or days per semi-standard accepted medical nomenclature. Counts days up until 59 days, then switches to 2 months. Returns values in months until 23 months, then returns years. Ignores timezones, etc. Just returns age in the (somewhat illogical) way we do it as humans.
 - `1.0.0`: Successfully consumed in both front end and back end packages. Considered stable.
 - `0.0.2`: Fix publishing issue.
 - `0.0.1`: First public release.
--- src/dob-to-age.js
+++ src/dob-to-age.js
@@ -1,34 +1,133 @@
 /**
+ * Extracts the local date string from a date object.
+ *
+ * If we get an arbitrary "local" date object from the browser, for say "new
+ * Date()" then the year, month, date, will all be in local time. But by turning
+ * it into a string we can then compare it to another timezone-less string like
+ * the dobString and get a consistent result.
+ *
  * @param {Date} date
  * @returns {string} As YYYY-MM-DD
  */
-const dateToString = date =>
-  [
-    date.getFullYear(),
-    (date.getMonth() + 1).toString().padStart(2, '0'),
-    date.getDate().toString().padStart(2, '0'),
-  ].join('-')
+const extractLocalDateString = date =>
+  [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+    .map(num => num.toString().padStart(2, '0'))
+    .join('-')
 
 /**
- * @param {string} dobString In format YYYY or YYYY-MM or YYYY-MM-DD
- * @returns {number} Number of years old
+ * A strange things happens in JS date constructor when you pass a string with
+ * the format 'YYYY-MM-DD'. It will create a date with the correct year, month,
+ * and day, but the time will be set to midnight in UTC.
+ *
+ * @param {string} dateString In format YYYY-MM-DD
+ * @returns {Date}
  */
-export default dobString => {
-  const now = new Date()
-  const currentYear = now.getFullYear()
-  // by creating both dates using the same mechanism
-  // we can safely compare them without worrying about
-  // timezones, etc.
-  const todayDate = new Date(dateToString(now))
+export const dateStringToDate = dateString => new Date(dateString)
+
+// if more than 59 days, switch to months
+const MAX_DAYS = 59
+// if we're at 24 months we always do years.
+const MAX_MONTHS = 23
+// ms in a day
+const MS_IN_DAY = 1000 * 60 * 60 * 24
+
+const acceptedDobRegex = /^\d{4}(-\d{2}){0,2}$/
+/**
+ * Calculates age in years, months, or days based on a date of birth string.
+ *
+ * @param {string} dobString - The date of birth string in format YYYY or
+ *   YYYY-MM or YYYY-MM-DD.
+ * @param {Date} [referenceDate] - The reference date to calculate age from
+ *   (usually the current date).
+ * @returns {{ years?: number; months?: number; days?: number }} - An object
+ *   containing the number of years, months, or days.
+ */
+export default (dobString, referenceDate) => {
+  if (typeof dobString !== 'string' || !acceptedDobRegex.test(dobString)) {
+    return {}
+  }
+  const dobParts = dobString.split('-')
+  if (dobParts.length === 1) {
+    dobString = `${dobString}-01-01`
+  } else if (dobParts.length === 2) {
+    dobString = `${dobString}-01`
+  }
+
+  const currentDateString = extractLocalDateString(referenceDate || new Date())
+
+  /**
+   * Normalize dates to avoid timezone differences. These will both be forced to
+   * be UTC because of how new Date() works in JS. If you don't have any time
+   * elements of a date string it will default to midnight UTC. This is a known
+   * quirky behavior of JS dates:
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#date_time_string_format
+   *
+   *
+   * But by converting both to UTC by first extracing "local" year/month/date
+   * and turning it into a string we can end up with two UTC dates we can safely
+   * compare them.
+   */
+  const currentDate = new Date(currentDateString)
   const birthDate = new Date(dobString)
 
-  // grabbing the number of years
-  const yearsOld = currentYear - birthDate.getFullYear()
+  /** Make sure we have two real dates */
+  if (isNaN(currentDate.valueOf())) {
+    return {}
+  }
+  if (isNaN(birthDate.valueOf())) {
+    return {}
+  }
 
-  // now we modify birthday to be same year as today
-  // so we can do a simple comparison to see if the
-  // day has occurred yet
-  birthDate.setFullYear(currentYear)
+  /**
+   * Calculate the raw # of days difference first. Flooring the resulting value
+   * ensures that we end up with zero indexing. Which is what we want because
+   * that's how we think about age in days. You're 0 days old on your birth day.
+   *
+   * This is the only part of the logic that uses the raw date values.
+   * Everything else is using our weird human logic for this type of thing.
+   */
+  const daysDifference = Math.floor(
+    (currentDate.valueOf() - birthDate.valueOf()) / MS_IN_DAY
+  )
 
-  return Math.max(birthDate > todayDate ? yearsOld - 1 : yearsOld, 0)
+  /**
+   * If the days difference is negative, we know the birth date is in the future
+   * so we treat this like other error conditions.
+   */
+  if (daysDifference < 0) {
+    return {}
+  }
+
+  /**
+   * If the days difference is less than the max days we return the days. But we
+   * never return a negative number of days.
+   */
+  if (daysDifference <= MAX_DAYS) {
+    return { days: daysDifference }
+  }
+
+  /**
+   * Now turn everything into numbers so we can do normal math without needing
+   * to do worry about timezones, etc.
+   */
+  const [currentYear, currentMonth, currentDay] = currentDateString
+    .split('-')
+    .map(Number)
+  const [birthYear, birthMonth, birthDay] = dobString.split('-').map(Number)
+
+  /** Calculate the number of months old */
+  let monthsOld = (currentYear - birthYear) * 12 + (currentMonth - birthMonth)
+
+  /** If the current day is less than the birth day, we need to subtract a month */
+  if (currentDay < birthDay) {
+    monthsOld--
+  }
+
+  /** If we're over 23 months we always show the values in years. */
+  if (monthsOld > MAX_MONTHS) {
+    return { years: Math.floor(monthsOld / 12) }
+  }
+
+  //  If not, we return the months.
+  return { months: monthsOld }
 }
